08e3ec7847e4d3da52e8881ba6f3603d8de54660
jeongseockhyeon <jeong9260@naver.com>
2024-06-09 17:31:22 +0900

    #4 Feat: 이미지 파일 저장 기능 수정

--- src/main/java/com/board_practice/board_practice/dto/BoardDTO.java
+++ src/main/java/com/board_practice/board_practice/dto/BoardDTO.java
@@ -22,7 +22,7 @@ public class BoardDTO {
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
 
-    private MultipartFile boardFile;
+    private MultipartFile boardFile; // 파일
     private String originalFileName; //원본 파일 이름
     private String storedFileName; // 서버 저장용 파일 이름
     private int fileAttached; // 파일 첨부 여부(첨부 1, 미첨부 0)
