57a50fdf892ede1d81ead59349a638ea507d16a5
jeongseockhyeon <jeong9260@naver.com>
2024-07-21 12:11:40 +0900

    #7 Feat: 댓글 작성 시 저장

--- src/main/java/com/board_practice/board_practice/controller/CommentController.java
+++ src/main/java/com/board_practice/board_practice/controller/CommentController.java
@@ -1,25 +1,35 @@
 package com.board_practice.board_practice.controller;
 
 import com.board_practice.board_practice.dto.CommentDTO;
+import com.board_practice.board_practice.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/comment")
 public class CommentController {
+    private final CommentService commentService;
 
-    @PostMapping("/save/{id}")
-    public @ResponseBody  ResponseEntity<?> commentSave(@ModelAttribute CommentDTO commentDTO){
+    @PostMapping("/save")
+    public @ResponseBody  ResponseEntity<?> commentSave(@RequestBody CommentDTO commentDTO){
         System.out.println("commentDTO = " + commentDTO);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body("댓글 작성");
+        Long saveResult = commentService.save(commentDTO);
+        if (saveResult != null){
+            List<CommentDTO> commentDTOList = commentService.findAll(commentDTO.getBoardId());
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(commentDTOList);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("해당 게시글이 존재하지 않습니다");
+        }
+
     }
 }
--- src/main/java/com/board_practice/board_practice/dto/CommentDTO.java
+++ src/main/java/com/board_practice/board_practice/dto/CommentDTO.java
@@ -1,5 +1,6 @@
 package com.board_practice.board_practice.dto;
 
+import com.board_practice.board_practice.entity.CommentEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,4 +16,15 @@ public class CommentDTO {
     private String commentContents;
     private Long boardId;
     private LocalDateTime commentCreatedTime;
+
+    public static CommentDTO toCommentDTO(CommentEntity commentEntity, Long boardId) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(commentEntity.getId());
+        commentDTO.setCommentWriter(commentEntity.getCommentWriter());
+        commentDTO.setCommentContents(commentEntity.getCommentContents());
+        commentDTO.setCommentCreatedTime(commentEntity.getCreatedTime());
+        commentDTO.setBoardId(boardId);
+
+        return commentDTO;
+    }
 }
